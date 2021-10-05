6a2e70525c34a54d6a822efbd29d15b932f60781
Clament <clament.john.k@gmail.com>
2021-10-05 13:31:11 +0530

    chore app: use buildSrc to manage dep versioning
    	Add hilt, navigation, coil, etc
    
    	We'll remove stuff later on

---
+++ easyLedger/buildSrc/src/main/java/dependencies/Versions.kt
@@ -0,0 +1,36 @@
+package dependencies
+
+object Versions {
+
+  const val compileSdk = 30
+  const val minSdk = 26
+  const val targetSdk = 30
+  const val versionCode = 1
+  const val versionName = "0.1"
+  const val jvmTarget = "1.8"
+
+  const val kotlin = "1.5.31"
+  const val gradle = "7.0.2"
+  const val hilt = "2.38.1"
+  const val junit5 = "1.8.0.0"
+
+  const val core_ktx = "1.6.0"
+  const val app_compat = "1.3.1"
+  const val constraint_layout = "2.1.1"
+  const val compose_ui = "1.0.3"
+  const val material = "1.4.0"
+  const val compose = "1.0.1"
+  const val compose_constraint = "1.0.0-rc01"
+  const val compose_activity = "1.4.0-beta01"
+  const val hilt_lifecycle_viewmodel = "1.0.0-alpha03"
+  const val nav_component = "2.3.5"
+  const val nav_compose = "2.4.0-alpha10"
+  const val hilt_navigation = "1.0.0-alpha03"
+  const val retrofit = "2.9.0"
+  const val okHttp = "4.9.0"
+  const val leak_canary = "2.7"
+  const val room =  "2.4.0-alpha05"
+  const val junit_jupiter = "5.7.0"
+  const val datastore = "1.0.0"
+  const val coil = "1.3.2"
+}
